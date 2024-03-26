33fdc6233f49b252c42cd91b48b334881ff08d42
Joe-Tao <btbutl@163.com>
2024-03-26 23:23:40 +0100

    add profile, but can't update image

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -4,7 +4,7 @@ import bcrypt from 'bcrypt';
 
 export async function POST(req) {
     const body = await req.json();
-    mongoose.connect(process.env.MONGO_URL);
+    mongoose.connect(process.env.MY_MONGO_URL);
     const pass = body.password;
     if (!pass?.length || pass.length < 5){
         new Error('password must be at least 5 characters');
